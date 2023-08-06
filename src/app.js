8f28efaf8c83a72cdf05ee5672bbee75d44bd869
Botaki555 <botakikebotshelo@gmail.com>
2023-08-06 21:07:29 +0200

    Added CSS and JS attributes

---
+++ src/app.js
@@ -0,0 +1,59 @@
+// Function for getting the current day, date and time
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let currentDay = days[now.getDay()];
+let currentHour = now.getHours();
+if (currentHour < 9) {
+  currentHour = `0${currentHour}`;
+}
+let currentMinute = now.getMinutes();
+if (currentMinute < 9) {
+  currentMinute = `0${currentMinute}`;
+}
+
+let currentDateTime = document.querySelector("#currentDateTime");
+currentDateTime.innerHTML = `${currentDay}, ${currentHour}:${currentMinute}`;
+
+function displayTemp(response) {
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector(
+    "#icon"
+  ).innerHTML = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png`;
+}
+
+function search(city) {
+  let apiKey = "bt404e491af877ff83o4902d86d401be";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searchCity").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#search");
+searchForm.addEventListener("submit", handleSubmit);
+
+search("Johannesburg");
